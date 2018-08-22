c50f4701edeb4b5bcaec5c1e536b205140729749
Mikhail Zakharov <mzkharo@gmail.com>
2018-08-22 17:30:33 -0400

    add schedule

--- ports/esp8266/modules/mon.py
+++ ports/esp8266/modules/mon.py
@@ -1,12 +1,39 @@
 import network
 import utime as time
 import os
-
-'''
+from micropython import const, schedule
 import machine
 rtc = machine.RTC()
-rtc.datetime((2018,8,22,2,11,18,0,0))
-'''
+rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+
+#rtc.datetime((2018,8,22,2,11,18,0,0))
+
+f = None
+
+def disable():
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.promiscuous_disable()
+    global f
+    ret = False
+    if f is not None:
+        f.close()
+        f = None
+        ret = True
+    print("Promiscuous mode disabled")
+    return ret
+
+
+MIN_15 = const(15*60) #guard to allow downloading files from internal flash
+MIN_30 = const(30*60) #guard to allow downloading files from internal flash
+TIMER_PERIOD=MIN_30
+boot_time = time.time()
+
+def sleep(seconds):
+    if ((time.time() - boot_time) > MIN_15 or machine.reset_cause() == machine.DEEPSLEEP_RESET):
+        if disable() is True: #only sleep if previously enabled
+            rtc.alarm(rtc.ALARM0, seconds * 1000)
+            print('put the device to sleep')
+            machine.deepsleep()
 
 def mac_to_str(a):
     return ("%02x:%02x:%02x:%02x:%02x:%02x" % (((a[0])), ((a[1])), ((a[2])),
@@ -16,9 +43,9 @@ def get_whm():
     t = time.localtime(time.time())
     return (t[6],t[3],t[4])
 
-f = None
 macs = {}
-def callback(mac_in):
+
+def monitor(mac_in):
     mac = mac_to_str(mac_in)
     if mac not in macs:
         if not mac_in[0] & 0x2:
@@ -30,8 +57,22 @@ def callback(mac_in):
         s ="{}-{}\n".format(whm,mac)
         print(s)
         f.write(s)
+        sleep(MIN_15)
 
-def enable(ch=6):
+def sleep_schedule(whm):
+    wkd,hour,_ = whm
+    if (hour >= 6 and hour <=20) and (wkd == 5 or wkd == 6 or hour > 16):
+        return False
+    return True #sleep sleep sleep
+
+
+def timer_schedule(self=None):
+    whm = get_whm()
+    print('{}-timer'.format(whm))
+    if sleep_schedule(whm):
+        sleep(MIN_30)
+
+def enable(ch=6, schedule=True):
     print("Promiscuous mode enable")
     sta_if = network.WLAN(network.STA_IF)
     sta_if.promiscuous_disable()
@@ -42,16 +83,15 @@ def enable(ch=6):
     global f
     f = open('log.txt', 'w+')
     f.write("{}-started\n".format(get_whm()))
-    sta_if.promiscuous_enable(callback)
+    sta_if.promiscuous_enable(monitor)
+    if schedule:
+        #tim = machine.Timer(-1)
+        #tim.init(period=TIMER_PERIOD * 1000, mode=machine.Timer.PERIODIC, callback=timer_schedule)
+        #timer causes crashes with monitor callbacks, things probably need to be in critical section, implement while True for now
+        while True:
+            timer_schedule()
+            time.sleep(TIMER_PERIOD)
 
-def disable():
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.promiscuous_disable()
-    global f
-    if f is not None:
-        f.close()
-        f = None
-    print("Promiscuous mode disabled")
 
 def clear():
     disable()
